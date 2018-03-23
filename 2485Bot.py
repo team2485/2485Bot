bcfde841ac4498affdc41c879d19fd93e87797f7
techied <dnelson2021@francisparker.org>
2018-03-23 09:55:12 -0700

    edited

--- 2485Bot.py
+++ 2485Bot.py
@@ -40,6 +40,7 @@ class S(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
         if("challenge" in post_data):
+            print(post_data[post_data.index("challenge") + 12:post_data[post_data.index("challenge") + 12:].index("}")-2])
             self.wfile.write(post_data[post_data.index("challenge") + 12:post_data[post_data.index("challenge") + 12:].index("}")-2])
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'rank':
             self._set_headers()
