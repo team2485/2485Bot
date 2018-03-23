74115823e09a80a7fcadd84bfbceebdd6daeb994
techied <dnelson2021@francisparker.org>
2018-03-23 09:01:41 -0700

    tba checking

--- 2485Bot.py
+++ 2485Bot.py
@@ -39,12 +39,14 @@ class S(BaseHTTPRequestHandler):
             # Print the status code of the response.
             print('STATUS CODE: ' + str(response.status_code))
             data = json.loads(response.text)
+            print('TBA RETURN: ' + data)
             if "ranking" in data:
                 self.wfile.write('Team 2485 is ranked ' + data["ranking"]["rank"])
             else:
                 self.wfile.write(data["overall_status_str"])
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'matches':
             print('Matches!')
+
 
 
 
