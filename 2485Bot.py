e40952c12f5a25f2cfac0ed44a76ff9bd4c7fa75
Nathan Sariowan <ncsariowan@gmail.com>
2018-03-29 10:19:03 -0700

    Trying matches

--- 2485Bot.py
+++ 2485Bot.py
@@ -53,24 +53,21 @@ class S(BaseHTTPRequestHandler):
         year = datetime.datetime.now().year
         event_key = str(year) + event
         print("YEAR ->>>>>>>>>>> " + event_key)
-
+        self._set_headers()
         if "channel_created" in post_data:
-            self._set_headers()
             print('channel id!!! : ' + post_data[post_data.index('{"id":"') + 7:post_data.index('","is_channel"')])
             sleep(1)
             doMessage(post_data[post_data.index('{"id":"') + 7:post_data.index('","is_channel"')], 'FIRST!!1!')
             self.wfile.write(200)
         elif "channel_unarchive" in post_data:
-            self._set_headers()
+
             print('channel id!!! : ' + post_data[post_data.index(',"channel":') + 12:post_data.index('","user"')])
             doMessage(post_data[post_data.index(',"channel":') + 12:post_data.index('","user"')], 'FIRST!!1!')
             self.wfile.write(200)
         elif "challenge" in post_data:
-            self._set_headers()
             print(post_data[post_data.index("challenge") + 12:post_data.index("}") - 2])
             self.wfile.write(post_data[post_data.index("challenge") + 12:post_data.index("}") - 2])
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'rank':
-            self._set_headers()
             response = getBlueAllianceResponse("/event/%s/status" % event_key)
             # Print the status code of the response.
             print('STATUS CODE: ' + str(response.status_code))
@@ -82,16 +79,15 @@ class S(BaseHTTPRequestHandler):
             else:
                 self.wfile.write(clear_b(data["overall_status_str"]))
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'matches':
-            self._set_headers()
             print('Matches!')
-            response = getBlueAllianceResponse("/event/%s/matches/simple" % event_key)
+            response = [Match(raw) for raw in self.getBlueAllianceResponse("/event/%s/matches/simple" % event_key)]
             # Print the status code of the response.
             print('STATUS CODE: ' + str(response.status_code))
             print(response.content)
             data = json.loads(response.text)
+            self.wfile.write('Team 2485 is in matches' + clear_b(data[]))
             print(data[""])
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'announcerank':
-            self._set_headers()
             response = getBlueAllianceResponse("/event/%s/status" % event_key)
             # Print the status code of the response.
             print('STATUS CODE: ' + str(response.status_code))
@@ -104,15 +100,12 @@ class S(BaseHTTPRequestHandler):
                 doMessage('C0A9JLBL2', clear_b(data["overall_status_str"]))
             self.wfile.write('Success!')
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'init-cheer':
-            self._set_headers()
             doMessage('C0A9JLBL2', 'WE ARE...')
             self.wfile.write('Success!')
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'cheer':
-            self._set_headers()
             doMessage('C0A9JLBL2', 'WARLORDS!')
             self.wfile.write('Success!')
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'cheera':
-            self._set_headers()
             doMessage('C0A9JLBL2', 'WARLORDA!')
             self.wfile.write('Success!')
 
