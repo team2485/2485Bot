8bc3d7830184dce8fe637fad3d2414308d691e0d
EC2 Default User <ec2-user@ip-172-31-6-159.us-west-1.compute.internal>
2018-03-31 20:55:35 +0000

    changes from shell

--- 2485Bot.py
+++ 2485Bot.py
@@ -86,7 +86,7 @@ class S(BaseHTTPRequestHandler):
             print(response.content)
             self.wfile.write('Team 2485 is in matches')
             data = json.loads(response.text)
-            for i in range(len(data):
+            for i in enumerate(data):
                  self.wfile.write(clear_b(data[i]["match_number"]))
             print(data[""])
         elif post_data[post_data.index('command=%2F') + 11:post_data.index('&text=')] == 'announcerank':
