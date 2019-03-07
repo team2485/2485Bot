21d55c9a9e03c9a71c2b7b9b0105fc9780a59227
Nathan Sariowan <ncsariowan@gmail.com>
2019-03-06 21:58:05 -0800

    Changes for Comp - nextmatch, lastmatch, purging out old things

--- TheBlueAlliance/TBA.py
+++ TheBlueAlliance/TBA.py
@@ -4,6 +4,6 @@ import os
 
 def request(request):
     headers = {'X-TBA-Auth-Key': os.environ['TBA_API_TOKEN']}
-    print("URL ->>> " + ("http://thebluealliance.com/api/v3/team/frc2485%s" % request))
-    return requests.get("http://thebluealliance.com/api/v3/team/frc2485%s" % request,
+    print("URL ->>> " + ("http://thebluealliance.com/api/v3" + request))
+    return requests.get("http://thebluealliance.com/api/v3" + request,
                         headers=headers)
